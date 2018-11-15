06d65e73405f4929d3a266ca72dc19c0eab670f0
Siddhartha Prasad <siprasa@microsoft.com>
2018-11-15 12:08:14 -0800

    Initial try, does not work

---
+++ Kubernetes/face/Face-Webapp/Face-Webapp/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Face_Webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Uri Endpoint = new Uri("http://face:5000");
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            string recognizedText = string.Empty;
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = formFile.OpenReadStream())
+                    {
+                        var lines = await ExtractTextSync(stream);
+                        recognizedText += "\n" + formFile.FileName + ": \n" + lines;
+                    }
+                }
+            }
+            return Ok(recognizedText);
+        }
+
+        private async Task<String> ExtractTextSync(Stream image)
+        {
+            String responseString = string.Empty;
+
+            using (var imageContent = new StreamContent(image))
+            using (var client = new HttpClient())
+            {
+                imageContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                client.BaseAddress = Endpoint;
+                var requestAddress = "/vision/v2.0/recognizetextDirect";
+
+                using (var response = await client.PostAsync(requestAddress, imageContent))
+                {
+                    var resultAsString = await response.Content.ReadAsStringAsync();
+                    var resultAsJson = JsonConvert.DeserializeObject<JObject>(resultAsString);
+
+                    if (resultAsJson["lines"] == null)
+                    {
+                        responseString = resultAsString;
+                    }
+                    else
+                    {
+                        foreach (var line in resultAsJson["lines"])
+                        {
+                            responseString += line["text"] + "\n";
+                        }
+                    }
+                }
+
+                return responseString;
+            }
+        }
+
+    }
+}
---
+++ Kubernetes/face/readme.md
@@ -0,0 +1,16 @@
+# Recognize Text
+
+Simple Kubernetes cluster with a frontend application that interfaces with the recognize text container.
+
+## Getting started
+
+1. Build and upload the front end image : `Recognize-Text-Webapp` to container registry.
+2. Set the frontend image value in `recognizetext.yml`.
+3. In `recognizetext.yml` under `imagePullSecrets`, replace `frontendregcred` with credentials created for your container registry. [More information on pulling from a private registry](https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/)
+
+4. Create registry credentials for the Container Preview registry, as detailed [here](https://thorsten-hans.com/how-to-use-a-private-azure-container-registry-with-kubernetes-9b86e67b93b6). Replace `regcred` in `imagePullSecrets` with these credentials.
+
+5. Run `kubectl create -f .\recognizetext.yml` to create the kubernetes services for the frontend and cognitive services container.
+6. You can now connect to `recognizetext-frontend` to interact with the frontend service. 
+    - Since `minikube` does not always support external IP addresses, you can get a public address for this service by running `minikube service recognizetext-frontend --url`.
+
