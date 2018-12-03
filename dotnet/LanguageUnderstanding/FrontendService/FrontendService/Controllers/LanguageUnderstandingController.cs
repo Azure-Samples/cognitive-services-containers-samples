94449baf3657856958ab5d362e70de5511b01fdc
brwals-msft <37676373+brwals-msft@users.noreply.github.com>
2018-12-03 15:08:55 -0800

    Add samples for LUIS service (#1)
    * Added LUIS samples
    
    * Added python sample
    
    * Fixed name references in documentation
    
    * renamed luis->languageunderstanding

---
+++ dotnet/LanguageUnderstanding/FrontendService/FrontendService/Controllers/LanguageUnderstandingController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Rest;
+
+namespace FrontendService.Controllers
+{
+    public class MockCredentials : ServiceClientCredentials {}
+
+    [Route("/")]
+    public class LanguageUnderstandingController : Controller
+    {
+        public static string ServiceEndpoint = "http://language-understanding:5000";
+        private const string APP_NAME = "da63910e-dddf-4c2f-a38f-a250a91ca176";
+        private ILUISRuntimeClient _luisClient;
+
+        public LanguageUnderstandingController()
+        {
+            _luisClient = new LUISRuntimeClient(new MockCredentials())
+            {
+                Endpoint = ServiceEndpoint
+            };
+        }
+
+        public string Get()
+        {
+            return "healthy";
+        }
+
+        // POST api/values
+        [HttpGet("{text}")]
+        public async Task<string> DetectLanguage([FromRoute] string text)
+        {
+            var result = await _luisClient.Prediction.ResolveAsync(APP_NAME, text);
+            return result.TopScoringIntent.Intent;
+        }
+    }
+}
