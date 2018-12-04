ea3d08ebc2173ab6ac38b8cea8db8ca9241394c6
Brendan Walsh <brwals@microsoft.com>
2018-12-04 14:39:13 -0800

    Added console app samples for luis and language

---
+++ dotnet/Language-Understanding/LanguageUnderstandingConsoleSdk/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+
+namespace LanguageUnderstandingConsole
+{
+    class Program
+    {
+        private const string Endpoint = "http://localhost:5000";
+        private const string ApplicationID = "da63910e-dddf-4c2f-a38f-a250a91ca176";
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage : \"<Phrase>\"");
+                Environment.Exit(-1);
+            }
+
+            var query = args[0];
+
+            var client = new LUISRuntimeClient(new ApiKeyServiceClientCredentials("None")) { Endpoint = Endpoint };
+            DetectIntent(client, query).Wait();
+        }
+
+        private static async Task DetectIntent(LUISRuntimeClient client, string query)
+        {
+            var prediction = await client.Prediction.ResolveAsync(ApplicationID, query);
+            Console.WriteLine($"Top Intent: {prediction.TopScoringIntent.Intent}");
+            Console.WriteLine();
+            Console.WriteLine($"Entities");
+            prediction.Entities.ToList().ForEach(e => Console.WriteLine($"   {e.Entity}"));
+        }
+    }
+}
---
+++ dotnet/Language/LanguageConsoleHttp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace LanguageConsoleHttp
+{
+    class Program
+    {
+        private const string Endpoint = "http://localhost:5000";
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage : <Phrase>");
+                Environment.Exit(-1);
+            }
+
+            var phrase = args[0];
+
+            var client = new HttpClient { BaseAddress = new Uri(Endpoint) };
+            DetectLanguage(client, phrase).Wait();
+        }
+
+        private static async Task DetectLanguage(HttpClient client, string phrase)
+        {
+            var content = new StringContent("{\"documents\":[{\"id\":\"1\", \"text\":\"" + phrase + "\"}]}");
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            
+            using (var rsp = await client.PostAsync("/text/analytics/v2.0/languages", content))
+            {
+                if (!rsp.IsSuccessStatusCode)
+                {
+                    var error = await rsp.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Request failed: {error}");
+                }
+                else
+                {
+                    var responseBody = await rsp.Content.ReadAsStringAsync();
+                    var responseJson = JToken.Parse(responseBody);
+                    Console.WriteLine($"Detected language: {responseJson["documents"][0]["detectedLanguages"][0]["name"]}");
+                }
+            }
+        }
+    }
+}
