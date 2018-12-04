e5ea2a3fa3e438e67327df3e4e7410c291be2430
brwals-msft <37676373+brwals-msft@users.noreply.github.com>
2018-12-03 16:17:12 -0800

    Added clarifying/guiding comments to luis samples (#2)
    * Added clarifying/guiding comments to luis samples
    
    * keeping log=true in sample LUIS query

--- dotnet/LanguageUnderstanding/FrontendService/FrontendService/Controllers/LanguageUnderstandingController.cs
+++ dotnet/LanguageUnderstanding/FrontendService/FrontendService/Controllers/LanguageUnderstandingController.cs
@@ -10,24 +10,42 @@ namespace FrontendService.Controllers
     [Route("/")]
     public class LanguageUnderstandingController : Controller
     {
+        // The hostname and port of the Kubernetes service which exposes the cognitive service container(s)
+        //
+        // Instead of hardcoding the hostname and port, environmental variables can be used to discover this address at runtime: 
+        // https://kubernetes.io/docs/concepts/containers/container-environment-variables/#cluster-information
         public static string ServiceEndpoint = "http://language-understanding:5000";
+
+        // The ID of your app/model.
+        // For more information about creating a new model see: https://docs.microsoft.com/en-us/azure/cognitive-services/luis/luis-how-to-start-new-app
         private const string APP_NAME = "da63910e-dddf-4c2f-a38f-a250a91ca176";
+
+        // The Client SDK which can be used to make requests to either your self-hosted cognitive service container or the cloud api.
         private ILUISRuntimeClient _luisClient;
 
         public LanguageUnderstandingController()
         {
             _luisClient = new LUISRuntimeClient(new MockCredentials())
             {
+                // Ensure the sdk client makes requests to your cognitive service containers instead of the cloud API
                 Endpoint = ServiceEndpoint
             };
         }
 
+        /// <summary>
+        /// A simple health endpoint used for availability and readiness monitoring in Kubernetes.
+        /// </summary>
+        /// <returns>Always returns "healthy" if the app is able.</returns>
         public string Get()
         {
             return "healthy";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Detects the language of text retrieved from the HTTP route.!--
+        /// Example:  http://<ip>:<port>/Hello World!
+        /// Response: English
+        /// </summary>
         [HttpGet("{text}")]
         public async Task<string> DetectLanguage([FromRoute] string text)
         {
--- python/LanguageUnderstanding/LanguageUnderstandingHttp/LanguageUnderstandingHttp.py
+++ python/LanguageUnderstanding/LanguageUnderstandingHttp/LanguageUnderstandingHttp.py
@@ -1,15 +1,14 @@
 import requests
 from pprint import pprint
 
+# The ID of your app/model.
+# For more information about creating a new model see: https://docs.microsoft.com/en-us/azure/cognitive-services/luis/luis-how-to-start-new-app
 language_understanding_app = "da63910e-dddf-4c2f-a38f-a250a91ca176"
 
 language_understanding_uri = "http://localhost:5000/luis/v2.0/apps/" + language_understanding_app
 
 query = { 
     'q': 'Book a flight to Cairo',
-    'staging': 'false',
-    'timezoneOffset': '0',
-    'verbose': 'false',
     'log': 'true'
 }
 
